7d110305bdcf358e2027ff1cbcf634963169b675
danonino25 <dananita2003mar@gmail.com>
2026-02-09 19:55:38 -0600

    feat: Configuración de controlador y rutas iniciales para el módulo Task

--- src/modules/tasks/tasks.controller.ts
+++ src/modules/tasks/tasks.controller.ts
@@ -1,11 +1,33 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { TaskService } from './tasks.service';
 
 @Controller('api/task')
 export class TaskController {
   constructor(private readonly taskSvc: TaskService) {}
-  @Get('1')
+
+  // ? http://localhost:3000/api/task
+  @Get()
   public fetchTasks(): string[] {
     return this.taskSvc.getTasks();
+  }
+
+  @Get(':id')
+  public getTaskById(@Param('id') id: number): any {
+    return this.taskSvc.getTaskById(id);
+  }
+
+  @Post()
+  public insertTask(task: any): any {
+    return this.taskSvc.insertTask(task);
+  }
+
+  @Put()
+  public updateTask(id: number, task: any): any {
+    return this.taskSvc.updateTask(id, task);
+  }
+
+  @Delete()
+  public deleteTask(id: number): any {
+    return this.taskSvc.deleteTask(id);
   }
 }
--- src/modules/tasks/tasks.service.ts
+++ src/modules/tasks/tasks.service.ts
@@ -5,4 +5,17 @@ export class TaskService {
   public getTasks(): string[] {
     return ['Task 1', 'Task 2', 'Task 3'];
   }
+
+  public getTaskById(id: number): string {
+    return `Se obtiene la tarea ${id}`;
+  }
+  public insertTask(task: any): any {
+    return task;
+  }
+  public updateTask(id: number, task: any): any {
+    return task;
+  }
+  public deleteTask(id: number): any {
+    return 'Eliminando la tarea ' + id;
+  }
 }
